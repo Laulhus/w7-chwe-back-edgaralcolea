3a13fa395286eb6e5b8ad94c5b8415070cacf82e
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-25 19:13:18 +0100

    Prepared scripts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
+const debug = require("debug")("myFriends:root");
 const chalk = require("chalk");
 const connectDatabase = require("./db/connectDatabase");
 const app = require("./server");
 const startServer = require("./server/startServer");
-const debug = require("debug")("myFriends:root");
+
 const port = process.env.PORT || 4000;
 const connectString = process.env.MONGO_CONNECT;
 

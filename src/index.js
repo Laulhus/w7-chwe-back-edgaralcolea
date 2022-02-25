b59f5c0bb6d6e549f67880fe1ced2629eaac71ae
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-25 18:42:45 +0100

    Created startServer.js

---
+++ src/index.js
@@ -0,0 +1,3 @@
+async () => {
+  startServer(app, port);
+};
---
+++ src/server/startServer.js
@@ -0,0 +1,11 @@
+const startServer = (app, port) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      debug(chalk.greenBright(`Server listening on http://localhost:${port}`));
+      resolve();
+    });
+
+    server.on("error", (error) => reject(error));
+  });
+
+module.exports = startServer;

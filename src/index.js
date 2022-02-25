b05e283555b34709fa05d96f3a2aab2effb3ca89
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-25 19:09:42 +0100

    Added eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const chalk = require("chalk");
 const connectDatabase = require("./db/connectDatabase");
-
+const app = require("./server");
+const startServer = require("./server/startServer");
+const debug = require("debug")("myFriends:root");
 const port = process.env.PORT || 4000;
 const connectString = process.env.MONGO_CONNECT;
 
